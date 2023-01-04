42f53f669f6f66147378fe274373bf3147d4906e
nick <nickadaniya@gmail.com>
2023-01-03 15:13:07 -1000

    please let it end

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -7,15 +7,24 @@ const { requireAuth } = require('../../utils/auth');
 
 router.delete('/:imageId', requireAuth, async (req, res) => {
     let foundImg = await ReviewImage.findByPk(req.params.imageId);
-    const newImg = foundImg.toJSON();
-    const foundReview = await Review.findByPk(newImg[0].reviewId);
-    if (newImg[0] && foundReview.userId === req.user.id) {
-        await foundImg.destroy();
-        res.statusCode = 200;
-        return res.json({
-            "message": "Successfully deleted",
-            "statusCode": 200
-        })
+    if (foundImg) {
+        const newImg = foundImg.toJSON();
+        console.log(newImg)
+        const foundReview = await Review.findByPk(newImg.reviewId);
+        if (foundImg && foundReview.userId === req.user.id) {
+            await foundImg.destroy();
+            res.statusCode = 200;
+            return res.json({
+                "message": "Successfully deleted",
+                "statusCode": 200
+            })
+        } else {
+            res.statusCode = 404;
+            return res.json({
+                "message": "Review Image couldn't be found",
+                "statusCode": 404
+            })
+        }
     } else {
         res.statusCode = 404;
         return res.json({
